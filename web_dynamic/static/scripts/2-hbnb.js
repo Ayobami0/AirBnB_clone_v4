58e5fc6b8b8fa5c14b8bdfdfa7c34db94e34e1e2
deelight-del <akingbenidavidoyin@gmail.com>
2024-02-09 16:50:09 +0000

    Task 3, unfruitful

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
-  const statusJson = $.get("http://0.0.0.0:5001/api/v1/status/");
   htmlStatus = $("#api_status");
-  if (statusJson["status"] == "OK") {
-    if (!htmlStatus.hasClass("available")) {
-      htmlStatus.addClass("available");
-    }
-  } 
-  else {
-    if (htmlStatus.hasClass("availble")) {
-      htmlStatus.removeClass("available");
-    }
-  }
-})
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus) {
+  if (data["status"] == "OK")
+    htmlStatus.addClass("available");
+  } else {
+    htmlStatus.removeClass("available");
+  });
+});
