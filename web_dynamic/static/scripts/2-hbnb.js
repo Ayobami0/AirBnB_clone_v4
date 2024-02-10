30b787476961cfc49ea25c15d083902ca87e3f3c
deelight-del <akingbenidavidoyin@gmail.com>
2024-02-10 11:29:23 +0000

    xxx

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   htmlStatus = $("#api_status");
   $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus) {
-  if (data["status"] == "OK")
+  if (data["status"] == "OK") {
     htmlStatus.addClass("available");
   } else {
     htmlStatus.removeClass("available");
+  }
   });
 });
