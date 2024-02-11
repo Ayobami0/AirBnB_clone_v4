fa48e48476d534aebf5de8c9b342f486c0e5c6fe
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-02-11 14:45:40 +0100

    fix(task3): Fix script not loading

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
-  htmlStatus = $("#api_status");
-  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus) {
-  if (data["status"] == "OK") {
-    htmlStatus.addClass("available");
-  } else {
-    htmlStatus.removeClass("available");
+window.addEventListener('load',
+  () => {
+    console.log('Ready');
+    const htmlStatus = $('#api_status');
+    $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+      console.log(data)
+      if (data.status === 'OK') {
+        htmlStatus.addClass('available');
+      } else {
+        htmlStatus.removeClass('available');
+      }
+    });
   }
-  });
-});
+);
